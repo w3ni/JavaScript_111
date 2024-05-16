595a2391649a393bd799013127a4807609ad406a
Weni Kr <152171489+w3ni@users.noreply.github.com>
2024-05-16 06:39:26 +0000

    container config done

---
+++ 01_basic.js
@@ -0,0 +1,13 @@
+const accountId = 123456;
+let accountEmail = "hitesh@google.com";
+var accountPassword = "12345";
+let accountCity = "Jaipur"; // Declare accountCity
+accountCity = "Jaipur"
+
+accountEmail = "h@hc.gmail.com";
+accountPassword = "11223344";
+accountCity = "Benguluru"; // Reassign accountCity
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
+// container config done
