d41d090e859c231b46b95fde93d632c7fbe68078
Weni Kr <152171489+w3ni@users.noreply.github.com>
2024-05-16 06:57:42 +0000

    datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict" // treat all js code as new version
+
+// alert(3+3) // we are using node js not browser
+
+console.log("Hello")
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false 
+
+// number = 2 to power 53
+// bigint 
+// string = '' , ""
+// boolean = true/false
+// null = standalone vlaue
+// undefined 
+// symbol = unique
+
+// object
+
+console.log(typeof age);
